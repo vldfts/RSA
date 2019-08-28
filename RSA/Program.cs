65dc29b52d66d27f59c13a0fba847e6747b7b8b3
vldf <vldfts@outlook.com>
2019-08-28 03:59:06 +0300

    Добавьте файлы проекта.

---
+++ RSA/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter a message: hex ");
+            //Console.Write("--> ");
+            //BigInteger mess = BigInteger.Parse(Console.ReadLine(), System.Globalization.NumberStyles.HexNumber);
+            User A = new User();
+            #region На сайті
+            //A.ReceiveKey();
+            //Console.WriteLine("Mesage: " + mess.ToString("X"));
+            //Console.WriteLine(A.Encrypt(mess).ToString("X"));
+            #endregion
+            #region на компі
+            A.GenerateKeyPair();
+            A.ReceiveKey();
+            A.SendKey();
+            A.Sign(BigInteger.Parse("13A3E39A2EEDB2F9", System.Globalization.NumberStyles.HexNumber));
+            //Console.WriteLine(A.Sign(1111).ToString("X"));
+            //Console.Write("Ciphertext: ");
+            Console.WriteLine(BigInteger.Parse("1CC89014A058A196BF967F36F35EECBE", System.Globalization.NumberStyles.HexNumber));
+            Console.WriteLine(BigInteger.Parse("9497E6740842438F04D830E3E14E714F", System.Globalization.NumberStyles.HexNumber));
+            //BigInteger mes = BigInteger.Parse(Console.ReadLine(), System.Globalization.NumberStyles.HexNumber);
+            //Console.WriteLine(A.Decrypt(mes).ToString("X"));
+            #endregion
+            #region протокол
+            //User B = new User();
+            //B.GenerateKeyPair();//отримувач
+            //do
+            //{
+            //    A.GenerateKeyPair();
+            //} while (A.n > B.n);
+            //Console.WriteLine("B");
+            //B.SendKey();
+            //Console.WriteLine(new string('_',50));
+            //Console.WriteLine("A");
+            //A.ReceiveKey();
+            //Console.WriteLine(new string('_', 50));
+            //Console.WriteLine("A");
+            //A.SendKey();
+            //Console.WriteLine(new string('_', 50));
+            //Console.WriteLine("B");
+            //B.ReceiveKey();
+            //Console.WriteLine(new string('_', 50));
+            //Console.WriteLine("Message: "+B.Decrypt(A.Encrypt(mess)).ToString("X"));
+            //Console.WriteLine("Verification: "+B.Verify(A.Sign(mess), A.Encrypt(mess)));
+            #endregion
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ RSA/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    class User : Realization
+    {
+        private BigInteger p { get; set; }
+        private BigInteger q { get; set; }
+        public BigInteger d { get; set; }
+        public BigInteger n { get; private set; }
+        public BigInteger n1 { get; set; }
+        public BigInteger e { get; private set; }
+        public BigInteger e1 { get; private set; }
+        public BigInteger k { get; private set; }
+        public BigInteger s { get; private set; }
+        public void GenerateKeyPair()
+        {
+
+            p = GetSimple(32, 4);
+            q = GetSimple(32, 4);
+            n = p * q;
+            e = 65537;
+            d = ModInverse(e, (p - 1) * (q - 1));
+        }
+        public BigInteger Encrypt(BigInteger message)
+        {
+            return BigInteger.ModPow(message, e1, n1);
+        }
+        public BigInteger Decrypt(BigInteger cipher)
+        {
+            return BigInteger.ModPow(cipher, d, n);
+        }
+        public BigInteger Sign(BigInteger message)
+        {
+            s= BigInteger.ModPow(message, d, n);
+            Console.WriteLine((s-n));
+            return BigInteger.ModPow(s, e1, n1);
+        }
+        public bool Verify(BigInteger signedMessage, BigInteger cryptMess)
+        {
+            if (Decrypt(cryptMess) == Encrypt(Decrypt(signedMessage))) 
+                return true;
+            return false;
+        }
+        public void SendKey()
+        {
+            Console.WriteLine("Send key:");
+            Console.WriteLine("N= " + n.ToString("X"));
+            Console.WriteLine("E= " + e.ToString("X"));
+        }
+        public void ReceiveKey()
+        {
+            Console.WriteLine("Receive key:");
+            Console.Write("N= ");
+            //n1 = BigInteger.Parse(Console.ReadLine());
+            n1 = BigInteger.Parse(Console.ReadLine(), System.Globalization.NumberStyles.HexNumber);
+            Console.Write("E= ");
+            //e1= BigInteger.Parse(Console.ReadLine());
+            e1 = BigInteger.Parse(Console.ReadLine(), System.Globalization.NumberStyles.HexNumber);
+        }
+
+    }
+}
